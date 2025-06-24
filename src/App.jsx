f52859c29f45871dff9710709faf33ac97ee844c
Guarotav <guarotav@gmail.com>
2025-06-24 18:40:31 -0400

    updated Deployed code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { createRoot } from "react-dom/client";
 // import axios from "axios"; // Maybe we'll need axios? 🤔
 import "./style.css";
+import SearchField from "./SearchField";
+import GifCard from "./GifCard";
+
+const GIPHY_API_KEY = `M5DpTGZFf9Mz3pKladW8ZWy50KnUi4d8`;
 
 // const GIPHY_API_KEY = "YOUR_API_KEY";
 
 const App = () => {
+  const [gifs, setGifs] = useState([]);
+  const [input, setInput] = useState("");
+
+  //Tries to fetch the data
+  //Copied the same function for fetchGifs and inserted the user 
+  const fetchTrendingGifs = async () => {
+  const url = `http://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`;
+  const response = await axios.get(url);
+  const trendingGifs = response.data.data;
+  setGifs(trendingGifs);
+};
+
+  const fetchGifs = async (searchTerm) => {
+    if (!searchTerm) {
+      console.log("No Search Term");
+    }
+    const url = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${GIPHY_API_KEY}`;
+    const gifsResponse = await axios.get(url);
+    const gifsData = gifsResponse.data;
+    const giphyData = gifsData.data;
+    setGifs(giphyData);
+  };
+
+  //useEffect to make sure that it does not go infinitley through that function
+  useEffect(() => {
+    fetchTrendingGifs(); // default search term when app loads
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with input:", input); //Tests to check if we were getting inputs
+    fetchGifs(input);
+  };
+
   return (
     <div className="app">
-      <h1 className="title">Let's Make Some API Requests!</h1>
+      <h1 className="title">Let's look for some Gifs!</h1>
+      <SearchField
+        input={input}
+        setInput={setInput}
+        handleSubmit={handleSubmit}
+      />
+      <button onClick={fetchTrendingGifs}>Trending GIFs</button>
+
+      <div className="gif-grid">
+        {gifs.map((gif) => (
+          <GifCard key={gif.id} gif={gif} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/GifCard.jsx
+++ src/GifCard.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const GifCard = () => {
-  return <div className="gif-card"></div>;
+const GifCard = (props) => {
+  const gif = props.gif;
+
+  const cardStyle = {
+    width: "200px",
+    borderRadius: "8px",
+    display: "inline-block",
+    margin: "8px", 
+  };
+
+  return (
+    <div className="gif-card">
+      <img
+        src={gif.images.fixed_height.url}
+        style={cardStyle}
+      />
+    </div>
+  );
 };
 
 export default GifCard;
