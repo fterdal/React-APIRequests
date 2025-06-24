ec8e448f4a3156c47f98700d00fea8728ecd8f51
Elian Echavarria <elianstanly431@gmail.com>
2025-06-24 16:19:48 -0400

    Elian - first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import axios from "axios"; // Maybe we'll need axios? 🤔
+import axios from "axios";
 import "./style.css";
-
+import GifCard from "./GifCard";
+import SearchField from "./SearchField";
 // const GIPHY_API_KEY = "YOUR_API_KEY";
+const apiKey = 'XFrizQa5xUa0QyIdpW7DVlGPZTC2p9m3';
+const url = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`
+
 
 const App = () => {
+  const [trending, setTrending] = useState([]);
+
+  const fetchGif = async () => {
+    try {
+      const response = await axios.get(url);
+      const gifData = response.data
+      setTrending(gifData.data);
+    } catch (err) {
+      console.error("Error fetching gifs:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchGif();
+  }, []);
+
+  console.log(trending);
+
   return (
-    <div className="app">
-      <h1 className="title">Let's Make Some API Requests!</h1>
+    <div className="gif-container">
+      <h1 className="title">Welcome to Giphy!</h1>
+      <div className="gif-input">
+        <SearchField data={trending} />
+      </div>
+      <div className="gif-grid">
+        {trending.map((gif) => (
+          <GifCard key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />
+        ))}
+      </div>
+
     </div>
   );
 };
 
-// The following lines initialize your React application and inject
-// it into the index.html
+
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
--- src/SearchField.jsx
+++ src/SearchField.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SearchField = () => {
-  return <div></div>;
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [apiGif, setApiGif] = useState([]);
+  const url = `https://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=XFrizQa5xUa0QyIdpW7DVlGPZTC2p9m3`;;
+
+
+
+  const dataFiltered = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setApiGif(data.data);
+      setFilteredData(data.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    dataFiltered();
+  }, [searchQuery])
+
+
+
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  }
+
+
+
+  return <div>
+    <input type="text"
+      placeholder="Search..."
+      value={searchQuery}
+      onChange={handleInputChange}
+      className="search-input"
+    />
+    <div className="gif-grid">
+      {filteredData.map((item) => (
+        <div className="gif-card" key={item.id}>
+          <img className="gif-img" src={item.images.fixed_height.url} alt={item.title} />
+        </div>
+      ))}
+    </div>
+
+
+
+  </div>;
 };
 
 export default SearchField;
